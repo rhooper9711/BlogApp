f5a8d44995f15bb0446a656fff6383ad9039a260
rhooper9711 <rhooper9711@gmail.com>
2017-04-19 12:14:53 -0500

    First Batch
    Couldn't upload node_modules?

---
+++ public/javascripts/BlogApp.js
@@ -0,0 +1,64 @@
+var app = angular.module('blogApp', ['ngResource', 'ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/add-blog', {
+            templateUrl: 'partials/blog-form.html',
+            controller: 'AddBlogCtrl'
+        })
+        .when('/blogs/delete/:id', {
+            templateUrl: 'partials/blog-delete.html',
+            controller: 'DeleteBlogCtrl'
+        })
+        .when('/ViewBlogs', {
+            templateUrl: 'ViewBlogs.html',
+            controller: 'BlogController'
+        })
+        .otherwise({
+            redirectTo: 'Home.html'
+        });
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource',
+    function ($scope, $resource) {
+        var Blogs = $resource('/api/blogs');
+        Blogs.query(function (blogs) {
+            $scope.blogs = blogs;
+        });
+    }]);
+
+
+app.controller('AddBlogCtrl', ['$scope', '$resource', '$location',
+    function ($scope, $resource, $location) {
+        $scope.save = function () {
+            var Blogs = $resource('/api/blogs');
+            Blogs.save($scope.blogs, function () {
+                $location.path('/');
+            });
+        };
+    }]);
+
+
+app.controller('DeleteBlogsCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function ($scope, $resource, $location, $routeParams) {
+        var Blogs = $resource('/api/blogs/:id');
+        Blogs.get({ id: $routeParams.id }, function (blogs) {
+            $scope.blogs = blogs;
+        })
+
+        $scope.delete = function () {
+            Blogs.delete({ id: $routeParams.id }, function (blogs) {
+                $location.path('/');
+            });
+        }
+    }]);
+
+
+
+
+
+
---
+++ routes/blog.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+var monk = require('monk');
+var db = monk('localhost:27017/BLOG_DB');
+
+router.get('/', function (req, res) {
+    var collection = db.get('blogs');
+    collection.find({}, function (err, blogs) {
+        if (err) throw err;
+        res.json(blogs);
+    });
+});
+
+
+router.get('/:id', function (req, res) {
+    var collection = db.get('blogs');
+    collection.findOne({ _id: req.params.id }, function (err, blogs) {
+        if (err) throw err;
+
+        res.json(blogs);
+    });
+});
+
+
+router.delete('/:id', function (req, res) {
+    var collection = db.get('blogs');
+    collection.remove({ _id: req.params.id }, function (err, blogs) {
+        if (err) throw err;
+
+        res.json(blogs);
+    });
+});
+
+router.post('/', function (req, res) {
+    var collection = db.get('blogs');
+    collection.insert({
+        title: req.body.title,
+        description: req.body.description,
+        by: req.body.author
+    }, function (err, blogs) {
+        if (err) throw err;
+
+        res.json(blogs);
+    });
+});
+
+module.exports = router;
+
+
+module.exports = app;
